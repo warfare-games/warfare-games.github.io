5bd93a00ab69cc54d75614c9b3024d5b8e906492
sergBull <bullsergv@gmail.com>
2020-09-05 20:41:01 +0300

    Первый запуск
    Первый день существования студии

---
+++ Main.js
@@ -0,0 +1,25 @@
+var feed = document.querySelector("section");
+
+function loadFeed(path) {
+	var xhr = new XMLHttpRequest();
+	
+	xhr.responseType = "document";;
+	
+	xhr.open("GET", path, true);
+	xhr.onload = function() {
+		var root = this.responseXML;
+		var posts = root.querySelectorAll("post");
+		
+		for (var i = 0; i < posts.length; i++) {
+			var post = document.createElement("article");
+			feed.appendChild(post);
+			
+			post.innerHTML = "<h1>" + posts[i].querySelector("title").innerHTML + "</h1>" +
+				"<p>" + posts[i].querySelector("description").innerHTML + "</p>";
+		}
+	};
+	 
+	xhr.send();
+}
+
+loadFeed("../assets/data/feed.xml");
